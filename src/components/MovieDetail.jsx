ef86758dbe1a4400690112cd141c83ccd6af260d
crisrodriguezgar <rodriguezgar.cris@gmail.com>
2023-10-22 18:04:50 +0200

    added styles details card

--- src/components/MovieDetail.jsx
+++ src/components/MovieDetail.jsx
@@ -1,22 +1,24 @@
 import '../styles/App.scss';
+import { Link } from 'react-router-dom';
 
 const MovieDetail = ({movieData}) => {
   return (
     <>
-      <ul>
-        <li className="card">
+      <ul className="details">
+        <li className="details__card">
           <img
-            className="card__img"
+            className="details__card__img"
             src={movieData.image}
             alt={`cartel de ${movieData.movie}`}
           />
-          <div className='card__textbox'>
-            <h4 className="card__textbox__title">{movieData.movie}</h4>
-            <p className="card__textbox__subtitle">"{movieData.quote}"</p>
-            <p className="card__textbox__director">{movieData.director}</p>
+          <div className='details__card__textbox'>
+            <h4 className="details__card__textbox__title">{movieData.movie}</h4>
+            <p className="details__card__textbox__subtitle">"{movieData.quote}"</p>
+            <p className="details__card__textbox__director"><i class="fa-solid fa-clapperboard fa-2xl details__card__textbox__icon"></i>{movieData.director}</p>
             
-            <a className="card__textbox__audio" href={movieData.audio} title="Ir al audio" target="_blank" rel="noreferrer"><i className="fa-solid fa-headphones"></i> Escuchar audio</a>
+            <a className="details__card__textbox__audio" href={movieData.audio} title="Ir al audio" target="_blank" rel="noreferrer"><i class="fa-solid fa-headphones fa-2xl details__card__textbox__icon"></i> Escuchar audio</a>
           </div>
+          <Link to="/"><i class="fa-solid fa-right-from-bracket fa-2xl details__card__textbox__exit"></i></Link>
         </li>
       </ul>
     </>
