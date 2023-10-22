22aa17b80aac775ae35cf194e900c45781a957bd
crisrodriguezgar <rodriguezgar.cris@gmail.com>
2023-10-22 18:49:39 +0200

    added responsive

--- src/components/MovieDetail.jsx
+++ src/components/MovieDetail.jsx
@@ -1,5 +1,5 @@
 import '../styles/App.scss';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
 const MovieDetail = ({movieData}) => {
   return (
@@ -11,14 +11,32 @@ const MovieDetail = ({movieData}) => {
             src={movieData.image}
             alt={`cartel de ${movieData.movie}`}
           />
-          <div className='details__card__textbox'>
+          <div className="details__card__textbox">
             <h4 className="details__card__textbox__title">{movieData.movie}</h4>
-            <p className="details__card__textbox__subtitle">"{movieData.quote}"</p>
-            <p className="details__card__textbox__director"><i class="fa-solid fa-clapperboard fa-2xl details__card__textbox__icon"></i>{movieData.director}</p>
-            
-            <a className="details__card__textbox__audio" href={movieData.audio} title="Ir al audio" target="_blank" rel="noreferrer"><i class="fa-solid fa-headphones fa-2xl details__card__textbox__icon"></i> Escuchar audio</a>
+            <p className="details__card__textbox__subtitle">
+              "{movieData.quote}"
+            </p>
+            <p className="details__card__textbox__director">
+              <i class="fa-solid fa-clapperboard fa-2xl details__card__textbox__icon"></i>
+              {movieData.director}
+            </p>
+
+            <a
+              className="details__card__textbox__audio"
+              href={movieData.audio}
+              title="Ir al audio"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i class="fa-solid fa-headphones fa-2xl details__card__textbox__icon"></i>{' '}
+              Escuchar audio
+            </a>
+            <div>
+              <Link to="/">
+                <i class="fa-solid fa-right-from-bracket fa-2xl details__card__textbox__exit"></i>
+              </Link>
+            </div>
           </div>
-          <Link to="/"><i class="fa-solid fa-right-from-bracket fa-2xl details__card__textbox__exit"></i></Link>
         </li>
       </ul>
     </>
